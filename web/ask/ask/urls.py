8bf79088ce0e5e252a921fd0aa6ebc3d9826211d
pythonbalm <78608269+pythonbalm@users.noreply.github.com>
2021-03-17 18:51:01 +0400

    Update urls.py

--- web/ask/ask/urls.py
+++ web/ask/ask/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', test),
     url(r'^login/', test),
     url(r'^signup/', test),
-    url(r'^ask/', test),
+    url(r'^ask', test),
     url(r'^popular/', test),
     url(r'^new/', test),
     url(r'^admin/', admin.site.urls),
